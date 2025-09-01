47c465982fa45d6fedc962bb7f6e77c0b061e386
Rumyantseva Julia <julia.rumyantseva@am-intech.ru>
2025-09-01 10:45:06 +0300

    удалены сборки

--- build/resources/main/db/migration/V2__add_project_data.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE estimates ADD COLUMN quality_level VARCHAR(16) DEFAULT 'standard';
-ALTER TABLE estimates ADD COLUMN status VARCHAR(32) DEFAULT 'Актуальный';
-ALTER TABLE estimates ADD COLUMN parameters JSON;
-ALTER TABLE estimates ADD COLUMN tasks JSON;
-
